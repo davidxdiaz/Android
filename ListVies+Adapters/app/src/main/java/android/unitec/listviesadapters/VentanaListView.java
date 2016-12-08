acb6a5d0518c01de780cbc717207495a56c92b54
davidxdiaz <davidxdiaz@unitec.edu>
2016-12-08 11:08:22 -0600

    agregado listview

---
+++ ListVies+Adapters/app/src/main/java/android/unitec/listviesadapters/VentanaListView.java
@@ -0,0 +1,13 @@
+package android.unitec.listviesadapters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VentanaListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_list_view);
+    }
+}
