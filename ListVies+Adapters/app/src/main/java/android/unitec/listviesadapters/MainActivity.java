9f0b16499990af08ab48b18a3eb5ffb31b4e6950
davidxdiaz <davidxdiaz@unitec.edu>
2016-12-08 16:15:58 -0600

    para que vayas viendo Miguel

--- ListVies+Adapters/app/src/main/java/android/unitec/listviesadapters/MainActivity.java
+++ ListVies+Adapters/app/src/main/java/android/unitec/listviesadapters/MainActivity.java
@@ -1,13 +1,23 @@
 package android.unitec.listviesadapters;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+    import android.os.Bundle;
+    import android.app.Activity;
+    import android.widget.ListView;
+    import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.listado);
+
+        ArrayList<Lista_entrada> datos = new ArrayList<Lista_entrada>();
+
+        datos.add(new Lista_entrada(R.drawable.Barcelona, "Barcelona", ""));
+        datos.add(new Lista_entrada(R.drawable.Bayern, "Bayern", ""));
+        datos.add(new Lista_entrada(R.drawable.Liverpool, "Liverpool", ""));
+        datos.add(new Lista_entrada(R.drawable.Madrid, "Real Madrid", ""));
+        datos.add(new Lista_entrada(R.drawable.milan, "Milan", ""));
     }
+
 }
